a5338e48a18cbe04826f01024b25bfdfb94ab56d
rahassou <rahassou@IUTCLINFA1213>
2023-09-21 09:42:19 +0200

    add structure

---
+++ Sources/AllInApi/Controllers/BetController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AllInApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BetController : ControllerBase
+    {
+    }
+}
