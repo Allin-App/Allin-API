45d45487b256050e829e6d4b577e6f15fcddf9ad
rahassou <rahassou@IUTCLINFC2104>
2023-09-22 14:15:47 +0200

    add users and group dto

--- Sources/AllInApi/DTO/GroupDTO.cs
+++ Sources/AllInApi/DTO/GroupDTO.cs
@@ -2,5 +2,8 @@
 {
     public class GroupDTO
     {
+        public string Name { get; set; }
+        public DateTime CreationDate { get; set; }
+        public LargeImageDTO Image { get; set; }
     }
 }
--- Sources/AllInApi/DTO/UserDTO.cs
+++ Sources/AllInApi/DTO/UserDTO.cs
@@ -3,6 +3,10 @@
     public class UserDTO
     {
         public string Name { get; set; }
+        public string Mail { get; set; }
         public LargeImageDTO Image { get; set; }
+        public int NumberOfCoins { get; set; }
+
+        public List<GroupDTO> Groups { get; set;}
     }
 }
