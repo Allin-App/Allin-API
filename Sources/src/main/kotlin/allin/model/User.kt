d10379286fde9b6634dfcf7aebcf9cfc974757d0
luevard <99143550+saucepommefrite@users.noreply.github.com>
2024-01-11 09:55:41 +0100

    :sparkles: Add database for local usage

--- Sources/src/main/kotlin/allin/model/User.kt
+++ Sources/src/main/kotlin/allin/model/User.kt
@@ -1,10 +1,10 @@
 package allin.model
 
 import allin.dto.UserDTO
+import allin.routing.users
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class User(val username: String, val email: String, var password: String, var nbCoins: Int = 1000, var token: String? = null)
-
 @Serializable
 data class CheckUser(val login: String,val password: String)
